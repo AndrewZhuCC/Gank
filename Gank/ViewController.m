205eebc5757411c7267795aed5a8deddc7f8d503
Andrew <zaz92537@126.com>
2016-06-27 16:49:41 +0800

    Initial Commit

---
+++ Gank/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Gank
+//
+//  Created by 朱安智 on 16/6/27.
+//  Copyright © 2016年 朱安智. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Gank/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Gank
+//
+//  Created by 朱安智 on 16/6/27.
+//  Copyright © 2016年 朱安智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
