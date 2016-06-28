6035b2b650ddda9a758430761298216042e8890b
Andrew <zaz92537@126.com>
2016-06-28 09:20:59 +0800

    add sdwebimage

---
+++ Gank/XXTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  XXTableViewCell.m
+//  Gank
+//
+//  Created by 朱安智 on 16/6/28.
+//  Copyright © 2016年 朱安智. All rights reserved.
+//
+
+#import "XXTableViewCell.h"
+
+@implementation XXTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ target 'Gank' do
 pod 'AFNetworking', '~> 3.0'
 pod 'Masonry'
 pod 'MJRefresh', '~> 3.1.9'
+pod 'SDWebImage', '~> 3.8.1'
 end
