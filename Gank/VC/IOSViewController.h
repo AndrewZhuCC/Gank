bc16d5e3f888dd08b45ec38060b2540af747778c
Andrew <zaz92537@126.com>
2016-06-28 17:38:37 +0800

    add ios

---
+++ Gank/VC/IOSViewController.h
@@ -0,0 +1,13 @@
+//
+//  IOSViewController.h
+//  Gank
+//
+//  Created by 朱安智 on 16/6/28.
+//  Copyright © 2016年 朱安智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSViewController : UIViewController
+
+@end
