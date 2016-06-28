f6245549bab02b947f7abe021eaa095409d45a99
Andrew <zaz92537@126.com>
2016-06-28 13:05:43 +0800

    add FDTemplateLayoutCell

---
+++ Gank/Model/GankResponse.h
@@ -0,0 +1,13 @@
+//
+//  GankResponse.h
+//  Gank
+//
+//  Created by 朱安智 on 16/6/28.
+//  Copyright © 2016年 朱安智. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GankResponse : NSObject
+
+@end
--- Podfile
+++ Podfile
@@ -5,4 +5,5 @@ pod 'AFNetworking', '~> 3.0'
 pod 'Masonry'
 pod 'MJRefresh', '~> 3.1.9'
 pod 'SDWebImage', '~> 3.8.1'
+pod 'UITableView+FDTemplateLayoutCell', '~> 1.4'
 end
